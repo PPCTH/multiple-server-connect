250af51cd82c7c882a55d9034d82324b0ecc2537
Blueoak <weravath.s@gmail.com>
2019-09-24 13:28:38 +0700

    - fix command error

--- DB2Connect.bat
+++ DB2Connect.bat
@@ -5,12 +5,12 @@ SET SERVERENV=%1
 SET ALTERFILE=%2
 
 
-IF NOT EXIST %SERVERENV%(
+IF NOT EXIST %SERVERENV% (
 	ECHO cannot file %SERVERENV% then start again.
 	GOTO :EOF
 )
 
-IF NOT EXIST %ALTERFILE%(
+IF NOT EXIST %ALTERFILE% (
 	ECHO cannot file %ALTERFILE% try to start again.
 	GOTO :EOF
 )
@@ -21,5 +21,5 @@ IF NOT EXIST %ALTERFILE%(
 
 FOR /F "tokens=1-3 delims=," %%i in (%SERVERENV%) do (
 	db2 -l %LOGFILE% connect to %%i user %%j using %%k
-	db2 -tvf %ALTERFILE%
+	db2 -l %LOGFILE% -tvf %ALTERFILE%
 )
