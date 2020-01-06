7f6a9c2d3e4efa9537e463e05a1f9513ac8ae0fd
Blueoak <weravath.s@gmail.com>
2020-01-06 09:28:19 +0700

    - commit a new version of project

--- DB2Connect.bat
+++ DB2Connect.bat
@@ -2,16 +2,16 @@
 
 SET LOGFILE=system.log
 SET SERVERENV=%1
-SET ALTERFILE=%2
+SET SQLLIST=%2
 
 
 IF NOT EXIST %SERVERENV% (
-	ECHO cannot file %SERVERENV% then start again.
+	ECHO cannot file %SERVERENV% then start again. >> %LOGFILE%
 	GOTO :EOF
 )
 
-IF NOT EXIST %ALTERFILE% (
-	ECHO cannot file %ALTERFILE% try to start again.
+IF NOT EXIST %SQLLIST% (
+	ECHO cannot file %SQLLIST% try to start again. >> %LOGFILE%
 	GOTO :EOF
 )
 
@@ -20,6 +20,26 @@ IF NOT EXIST %ALTERFILE% (
 :PROCESS
 
 FOR /F "tokens=1-3 delims=," %%i in (%SERVERENV%) do (
+	
+	
+	ECHO -------- CONNECT TO %%i --------- >> %LOGFILE%
 	db2 -l %LOGFILE% connect to %%i user %%j using %%k
-	db2 -l %LOGFILE% -tvf %ALTERFILE%
+
+	FOR /F "tokens=*" %%a in (%SQLLIST%) do (
+
+		SET SQLFILE=SQL/%%a
+		ECHO ------------------------------------ >> %LOGFILE%
+		ECHO -- PREPARE TO PROCESS %%a -- >> %LOGFILE%
+		IF NOT EXIST %SQLFILE% (
+			ECHO cannot file %SQLFILE% try to start again. >> %LOGFILE%
+			GOTO :EOF
+		)
+		db2 -l %LOGFILE% -tvf %SQLFILE%
+		db2 terminate
+		
+		ECHO --- FINISTH PROCESS %%a ---- >> %LOGFILE%
+		ECHO ------------------------------------ >> %LOGFILE%
+
+	)
+
 )
